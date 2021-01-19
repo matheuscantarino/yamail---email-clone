780bed70203e4d52b741f23c3afdef3b2f3cea65
Matheus <mcantarinojf@gmail.com>
2021-01-19 00:02:47 -0300

    main

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
-echo 'oi';
-echo 'sla';
 ?>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./css/main.css">
+        <title>yA-Mail - Mailbox</title>
+    </head>
+    <body>
+        <div class = "boxbody">
+            <div class = "leftbox">
+                <h2>yA-Mail</h2><hr>
+            </div>
+            <div class = "boxmessages">
+            </div>
+        </div>
+    </body>
+</html>
